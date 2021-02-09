b0240b39ba4e30c07f3a51e5f039cf415fff076b
Andrew Curd <andrewcurd@Andrews-MacBook-Pro.local>
2021-02-09 08:43:07 -0800

    added some styling to login/signup form

--- frontend/components/session/session_form.jsx.jsx
+++ frontend/components/session/session_form.jsx.jsx
@@ -28,24 +28,30 @@ class SessionForm extends React.Component{
         return(
             <div id="session_div">
             <form onSubmit={this.handleSubmit} id="session_form">
-                <h3>{formType}</h3>
-                <label id="session_username"> Username:
+                <h3 className="session_row">{formType}</h3>
+                    <label id="session_username" className="session_row"> Username:
                 <br/>
                     <input 
+                        id="session_user_input"
+                        className="session_row"
                         type="text" value={this.state.username} 
                         onChange={this.handleCahnge("username")}
+                        placeholder="Username"
                         />
                 </label>
                 <br/>
-                <label id="session_username"> Password:
+                <label id="session_password"> Password:
                 <br/>
                     <input
+                        id="session_password_input"
+                        placeholder="Password"
+                        className="session_row"
                         type="password" value={this.state.password}
                         onChange={this.handleCahnge("password")}
                     />
                 </label>
                 <br/>
-                <button type="submit" id="session_submit">
+                    <button type="submit" id="session_submit" className="session_row">
                     {formType}
                 </button>
             </form>
