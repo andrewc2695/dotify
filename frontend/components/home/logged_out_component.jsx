1bf14ac2cf0bdc2996b5432199fbf95c28e287b0
Andrew Curd <andrewcurd@Andrews-MacBook-Pro.local>
2021-02-10 14:29:56 -0800

    added in font awesome and finsihed spalsh screen

--- frontend/components/home/logged_out_component.jsx
+++ frontend/components/home/logged_out_component.jsx
@@ -1,9 +1,26 @@
 import React from "react"
 import { Link, Redirect, Route } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons'
+
+
 
 
 class LoggedOutComponet extends React.Component{
-    
+    constructor(props){
+        super(props)
+        this.state = {modal: "loggout_modal_hidden"}
+    }
+
+
+handleModal(){
+    if (this.state.modal === "loggout_modal_hidden"){
+        this.setState({modal: "loggout_modal"})
+    }else{
+        this.setState({modal: "loggout_modal_hidden"})
+    }
+}
 
     render(){
         return(
@@ -17,9 +34,10 @@ class LoggedOutComponet extends React.Component{
                             <Link to="/signup">Sign Up</Link>
                             <Link to="login">Login</Link>
                         </div>
-                        <div className="website_header_div_div" id="modal_button_div">
-                            button
-                        </div>
+                        <button className="website_header_div_div" id="modal_button_div"
+                            onClick={() => this.handleModal()}>
+                            <FontAwesomeIcon icon={faBars} />
+                        </button>
                     </div>
                 </header>
                 <div className="logged_out_div">
@@ -37,11 +55,12 @@ class LoggedOutComponet extends React.Component{
                             Sign Up
                         </Link>
                     </div>
-                    <div id="loggout_modal">
-                        <div id="modal_content">
-                            <div id="modal_link_div">
-                                hi
+                    <div id={this.state.modal} onClick={() => this.handleModal()}>
+                        <div className="website_header_div_div" id="modal_content">
+                            <button id="modal_close" onClick={() => this.handleModal()}><FontAwesomeIcon icon={faWindowClose} /></button>
+                            <div id="modal_links">
                                 <Link to="/signup">Sign Up</Link>
+                                <hr id="modal_line" />
                                 <Link to="login">Login</Link>
                             </div>
                         </div>
