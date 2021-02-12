2c39e5991741fc3b43dc4a563bbd8c33717a34f2
Andrew Curd <andrewc2695@gmail.com>
2021-02-11 22:48:15 -0800

    styled head nav bar

--- frontend/components/home/header/header_component.jsx
+++ frontend/components/home/header/header_component.jsx
@@ -1,9 +1,24 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronLeft, faChevronRight, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft, faChevronRight, faSearch, faGrin, 
+          faSortDown, faSortUp } from '@fortawesome/free-solid-svg-icons'
 
 
 class HeaderComponent extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = { dropdownButton: "header_dropdown_button_down", sort: faSortDown}
+        this.handleDropDown = this.handleDropDown.bind(this);
+    }
+
+    handleDropDown(){
+        if (this.state.dropdownButton === "header_dropdown_button_down"){
+            this.setState({ dropdownButton: "header_dropdown_button_up", sort: faSortUp})
+        }else{
+            this.setState({ dropdownButton: "header_dropdown_button_down", sort: faSortDown})
+        }
+    }
+
     render(){
         return(
             <div id="header_component">
@@ -22,9 +37,19 @@ class HeaderComponent extends React.Component{
                     </div>
                 </div>
                 <div id="header_users_div">
-                    <div id="header_users">
+                    <div id="header_profile">
+                        <FontAwesomeIcon icon={faGrin} className="profile_icon" />
 
                     </div>
+                    <div id="header_user_name">
+                        AndrewCurd
+                    </div>
+                    <button id="header_dropdown_button" onClick={this.handleDropDown}>
+                        <FontAwesomeIcon icon={this.state.sort} className={this.state.dropdownButton} />
+                    </button>
+                </div>
+                <div id="dropdown_menue_visable">
+
                 </div>
             </div>
         )
